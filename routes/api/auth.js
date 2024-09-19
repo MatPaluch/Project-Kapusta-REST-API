dac243d6d10eeed7bcadea86c0d4d75e29ef433e
MatPaluch <xp.mateusz@gmail.com>
2024-09-19 16:46:43 +0200

    init

---
+++ routes/api/auth.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.post("/register", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+router.post("/login", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+router.post("/logout", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,6 @@
+const app = require("./app");
+
+app.listen(8000, () => {
+  console.log("Server is running.");
+  console.log("Use our API on port: http://localhost:8000/");
+});

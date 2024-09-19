a691945776b6c76dcc0e449ce08b6a9702391ff5
MatPaluch <xp.mateusz@gmail.com>
2024-09-19 17:57:48 +0200

    BIG_INI

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const app = require("./app");
+const mongoose = require("mongoose");
 
-app.listen(8000, () => {
-  console.log("Server is running.");
-  console.log("Use our API on port: http://localhost:8000/");
-});
+require("dotenv").config();
+
+const URI_DB = process.env.DB_HOST;
+const connection = mongoose.connect(URI_DB);
+
+connection
+  .then(() => {
+    console.log("Database connected successfully!");
+
+    app.listen(8000, () => {
+      console.log("Server is running.");
+      console.log("Use our API on port: http://localhost:8000");
+    });
+  })
+  .catch((error) => {
+    console.log(
+      `Something gone wrong with connection with DB. Error:${error.message}`
+    );
+    process.exit(1);
+  });

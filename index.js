fab84c086d0d442f4d95249ab773821e6fdf3c0e
Mateusz Paluszkiewicz <150561295+MatPaluch@users.noreply.github.com>
2024-09-21 18:57:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,23 +3,10 @@ const mongoose = require("mongoose");
 
 require("dotenv").config();
 
-const URI_DB = process.env.DB_HOST;
-const connection = mongoose.connect(URI_DB);
-
-connection
-  .then(() => {
-    console.log("Database connected successfully!");
-    app.get("/", async (req, res) => {
+ app.get("/", async (req, res) => {
       res.json({ message: "NICE!" });
     });
     app.listen(3000, () => {
       console.log("Server is running.");
       console.log("Use our API on port: http://localhost:3000");
     });
-  })
-  .catch((error) => {
-    console.log(
-      `Something gone wrong with connection with DB. Error:${error.message}`
-    );
-    process.exit(1);
-  });

f263426b4a4907e524549c6a0e7bb65c3a1b7ad6
MatPaluch <xp.mateusz@gmail.com>
2024-09-22 12:54:21 +0200

    and__this

--- api/app.js
+++ api/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
-const authRouter = require("./routes/api/auth.js");
+const authRouter = require("../routes/api/auth.js");
 
 const app = express();
 

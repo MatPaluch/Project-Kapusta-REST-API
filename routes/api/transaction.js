d6308138a835b597fb92982500a437f7d73dc3f4
MatPaluch <xp.mateusz@gmail.com>
2024-09-24 22:05:59 +0200

    Corrections

--- routes/api/transaction.js
+++ routes/api/transaction.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const authenticate = require('../controllers/auth/authenticateJWT');
 const addExpense = require('../controllers/transaction/addExpense');
 const addIncome = require('../controllers/transaction/addIncome');
-const transactionController = require('../controllers/transaction/transactionController');
-const transactionRoutes = require('../controllers/transaction/transactionRoutes');
+const deleteTransaction = require('../controllers/transaction/transactionController');
 
 router.post('/expense', authenticate, addExpense);
 router.post('/income', authenticate, addIncome);
+router.delete('/:id', authenticate, deleteTransaction);
 
 module.exports = router;
--- routes/controllers/transaction/transactionRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { deleteTransaction } = require('../transaction/transactionController');
-
-router.delete('/transactions/:id', deleteTransaction);
-
-module.exports = router;

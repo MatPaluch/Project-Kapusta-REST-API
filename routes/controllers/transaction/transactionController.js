371ba435f98a4516d491df48f6e54d45a7815dd0
Piotr Frontczak <piotrf2023@gmail.com>
2024-09-25 22:01:15 +0200

    b11 created

--- routes/controllers/transaction/transactionController.js
+++ routes/controllers/transaction/transactionController.js
@@ -15,4 +15,34 @@ const deleteTransaction = async (req, res) => {
     }
 };
 
-module.exports = { deleteTransaction };
+const getMonthlyExpenses = async (req, res) => {
+    try {
+        const currentYear = new Date().getFullYear();
+        const expenses = await Transaction.aggregate([
+            {
+                $match: {
+                    type: 'expense',
+                    date: {
+                        $gte: new Date(`${currentYear}-01-01`),
+                        $lt: new Date(`${currentYear + 1}-01-01`)
+                    }
+                }
+            },
+            {
+                $group: {
+                    _id: { $month: '$date' },
+                    totalExpenses: { $sum: '$amount' }
+                }
+            },
+            {
+                $sort: { _id: 1 }
+            }
+        ]);
+
+        return res.status(200).json({ data: expenses });
+    } catch (error) {
+        return res.status(500).json({ message: 'Server error', error });
+    }
+};
+
+module.exports = { deleteTransaction, getMonthlyExpenses };
--- routes/controllers/transaction/transactionRoutes.js
+++ routes/controllers/transaction/transactionRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { deleteTransaction } = require('../transaction/transactionController');
+const { deleteTransaction, getMonthlyExpenses } = require('../transaction/transactionController');
 
 router.delete('/transactions/:id', deleteTransaction);
+router.get('/transactions/monthly-expenses', getMonthlyExpenses);
 
 module.exports = router;

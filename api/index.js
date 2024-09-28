50246597a33f707a795df11eac6b3fde4059652c
MatPaluch <xp.mateusz@gmail.com>
2024-09-28 13:24:27 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,23 +1,21 @@
-require("dotenv").config();
+const app = require('./app');
+const mongoose = require('mongoose');
 
-const app = require("./app");
-const mongoose = require("mongoose");
+require('dotenv').config();
 
 const URI_DB = process.env.DB_HOST;
 const connection = mongoose.connect(URI_DB);
 
 connection
   .then(() => {
-    console.log("Database connected successfully!");
+    console.log('Database connected successfully!');
 
     app.listen(3000, () => {
-      console.log("Server is running.");
-      console.log("Use our API on port: http://localhost:3000");
+      console.log('Server is running.');
+      console.log('Use our API on port: http://localhost:3000');
     });
   })
-  .catch((error) => {
-    console.log(
-      `Something gone wrong with connection with DB. Error:${error.message}`
-    );
+  .catch(error => {
+    console.log(`Something gone wrong with connection with DB. Error:${error.message}`);
     process.exit(1);
   });

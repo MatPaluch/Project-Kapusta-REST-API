103434ecabd8cc732bfebb424dadef2d81d2ce0a
MatPaluch <xp.mateusz@gmail.com>
2024-10-13 21:30:53 +0200

    Delete corrections

--- routes/controllers/transaction/deleteTransaction.js
+++ routes/controllers/transaction/deleteTransaction.js
@@ -1,15 +1,30 @@
 const Transaction = require('../../../models/mongoose/transactionSchema');
+const User = require('../../../models/mongoose/userSchema');
 
 const deleteTransaction = async (req, res) => {
   try {
     const { id } = req.params;
-    const transaction = await Transaction.findByIdAndDelete(id);
+
+    const transaction = await Transaction.findById(id);
 
     if (!transaction) {
       return res.status(404).json({ message: 'Transaction not found' });
     }
 
-    return res.status(200).json({ message: 'Transaction deleted successfully' });
+    await Transaction.findByIdAndDelete(id);
+
+    const user = await User.findById(transaction.userId);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    user.balance += transaction.amount;
+    await user.save();
+
+    return res
+      .status(200)
+      .json({ message: 'Transaction deleted and balance updated successfully' });
   } catch (error) {
     return res.status(500).json({ message: 'Server error', error });
   }
